0c3b42973e9ab612c084d2fb3f5e279fb1e1f740
Kalpan Bhatt <Kalpan.bhatt@gmail.com>
2013-09-20 02:11:08 -0400

    RecursionProblems

---
+++ StringRecursion.java
@@ -0,0 +1,38 @@
+
+public class StringRecursion {
+	
+	public static void printReverse(String str){
+		if (str == null || str.length() == 0) return;
+		
+	}
+	
+	public static String reverse(String str){
+		if (str == null || str.length() == 0) return str;
+		int lnth = str.length();
+		if (lnth ==1) return str;
+		if (lnth ==2) return str.substring(1,2)+str.substring(0,1);
+
+		return str.substring(lnth-1,lnth) + reverse(str.substring(1,lnth-1)) + str.substring(0,1);
+		
+	}
+	
+	public static String trim(String str){
+		if (str == null || str.length() == 0) return str;
+		
+		if (!str.substring(0,1).equals(" ") && !str.substring(str.length()-1,str.length()).equals(" ")) return str;
+		if (str.substring(0,1).equals(" ") && str.substring(str.length()-1,str.length()).equals(" ") && str.length()>1) return trim(str.substring(1,str.length()-1));
+		if (str.substring(0,1).equals(" ")) return trim(str.substring(1,str.length()));
+		else return trim(str.substring(0,str.length()-1));
+	}
+	
+	
+	public static void main(String args[]){
+		String none = null;
+		String blank = "";
+		System.out.println(trim("    "));
+		
+		System.out.println(reverse("KalpanBhatt"));
+		//printReverse("Kalpanbhatt");
+		
+	}
+}
